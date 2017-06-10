d49082907aa7d00101ca66d17abe6ef321503bad
William Lopez <william_lopez1@usmp.pe>
2017-06-10 03:01:34 -0500

    prueba

--- 2011600136-SLN/2011600136-ENT/IRepositories/IRepository.cs
+++ 2011600136-SLN/2011600136-ENT/IRepositories/IRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _2011600136_ENT.IRepositories
 {
-    public interface IRepository
+    public interface IRepository <TEntity> where TEntity : class
     {
     }
 }
--- 2011600136-SLN/2011600136-PER/Repositories/CalificacionHospedajeRepository.cs
+++ 2011600136-SLN/2011600136-PER/Repositories/CalificacionHospedajeRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _2011600136_PER.Repositories
 {
-    class CalificacionHospedajeRepository
+    public class CalificacionHospedajeRepository
     {
     }
 }
