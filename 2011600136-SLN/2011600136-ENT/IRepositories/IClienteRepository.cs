f18bff2e5b8e5fe5c1f501289b5588fb98efe020
William Lopez <william_lopez1@usmp.pe>
2017-06-10 03:08:58 -0500

    edicion de IREPOSITORY

--- 2011600136-SLN/2011600136-ENT/IRepositories/IClienteRepository.cs
+++ 2011600136-SLN/2011600136-ENT/IRepositories/IClienteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _2011600136_ENT.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace _2011600136_ENT.IRepositories
 {
-    public interface IClienteRepository : IRepository
+    public interface IClienteRepository : IRepository<Cliente>
     {
     }
 }
